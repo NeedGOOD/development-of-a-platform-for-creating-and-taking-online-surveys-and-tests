7dca7b8daf0cd40ed851def2d0f48c51902a6fe2
NeedGOOD <vladyakusevich@gmail.com>
2026-04-27 11:51:01 +0200

    feat: implement AuthGuard for JWT authentication in AuthController

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, HttpCode, HttpStatus, NotImplementedException, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,11 @@ export class AuthController {
     @Post('login')
     login(@Body() input: { email: string, password: string }) {
         return this.authService.authenticate(input);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('me')
+    getUserInfo(@Request() request: any) {
+        return request.user;
     }
 }
